8aca334b96cbc92d32096fd0d87cb4a65e8d3f25
IsaacDouglas-SE <isaac.douglas@nationwide.co.uk>
2019-10-24 20:39:11 +0100

    My backend controllers in IDE with SpringBoot

---
+++ src/main/java/fifa/packages/main/Data/Fifa.java
@@ -0,0 +1,75 @@
+package fifa.packages.main.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fifa {
+	
+	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long id;
+	private String playername;
+	private String position,league,club,nationality,quality;
+	private int rating;
+	private int price;
+
+//	public Long getId() {
+//		return id;
+//	}
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+	public String getPlayername() {
+		return playername;
+	}
+	public void setPlayername(String playername) {
+		this.playername = playername;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getLeague() {
+		return league;
+	}
+	public void setLeague(String league) {
+		this.league = league;
+	}
+	public String getClub() {
+		return club;
+	}
+	public void setClub(String club) {
+		this.club = club;
+	}
+	public String getNationality() {
+		return nationality;
+	}
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	public String getQuality() {
+		return quality;
+	}
+	public void setQuality(String quality) {
+		this.quality = quality;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	}
+	
---
+++ src/main/java/fifa/packages/main/mainapp.java
@@ -0,0 +1,13 @@
+package fifa.packages.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mainapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(mainapp.class, args);
+	}
+
+}
